e6537297f147d478df163b5c77c0f0fd42829fc8
Olha <lol79lol97@gmail.com>
2024-01-04 00:45:11 +0200

    Add jest check github action"

--- BE/src/signup/signup.controller.spec.ts
+++ BE/src/signup/signup.controller.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { SignupController } from './signup.controller';
 
 describe('SignupController', () => {
-  let controller: SignupController;
+  let Controller: SignupController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SignupController],
     }).compile();
 
-    controller = module.get<SignupController>(SignupController);
+    Controller = module.get<SignupController>(SignupController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(Controller).toBeDefined();
   });
 });
